9eb50162be5c9b913321a0bcaaf3489cee62bcca
orneladjouhou <orneladjouho@gmail.com>
2024-01-29 13:59:49 -0500

    newcode

---
+++ teraform/main.tf
@@ -0,0 +1,11 @@
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
